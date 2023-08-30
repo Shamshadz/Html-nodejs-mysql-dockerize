e154f1675ffc82283cc7bc1fd549727e16b24929
shamshadz <chaudhary.s.shamshad07@gmail.com>
2023-08-31 00:09:01 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const { body, validationResult } = require('express-validator');
 const app = express();
 const PORT = process.env.PORT || 3000; // Default to port 3000 if PORT is not provided in the environment variables
@@ -8,9 +8,18 @@ const PORT = process.env.PORT || 3000; // Default to port 3000 if PORT is not pr
 require('dotenv').config();
 
 const database = mysql.createConnection({
+    host: process.env.DB_HOST, // Change to the IP or hostname if MySQL is running in a different container
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+});
+
+database.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
 });
 
 app.get('/init', (req, res) => {
@@ -29,6 +38,7 @@ app.post('/subscribe',
     body('lastname').not().isEmpty().escape(),
     (req, res) => {
         const errors = validationResult(req);
+        console.log(req.body);
 
         if (errors.array().length > 0) {
             res.send(errors.array());
@@ -48,6 +58,17 @@ app.post('/subscribe',
             });
         }
     });
+
+
+app.get('/', (req, res) => {
+    const sqlQuery = 'SELECT * FROM emails';
+
+    database.query(sqlQuery, (err, result) => {
+        if (err) throw err;
+
+        res.json({ 'emails': result });
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
